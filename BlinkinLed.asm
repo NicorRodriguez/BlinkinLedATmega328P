b17b6edd2ad3217aec9cb720cb71a2361a2846a7
NicorRodriguez <nicolas@192.168.1.11>
2022-09-06 23:46:36 -0300

    Blinking led.

--- BlinkinLed.asm
+++ BlinkinLed.asm
@@ -13,36 +13,45 @@ out DDRB,r16; configuro el puerto para para que los pines 5 al 2 sean salida
 ldi r16,0b11111011; ;
 out PORTB,r16;
 
-start: 
-	ldi r17,0xFF; cargo valor para delay0
-
 main:
+    ldi r17,0xFF
+    ldi r18,0xFF
     led0:
         ldi r16,0b11111011
         out PORTB,r16
     delay0:
         dec r17
-        brne delay0;
+        brne delay0
+            dec r18
+            brne delay0
 	ldi r17,0xFF
+	ldi r18,0xFF
     led1:
         ldi r16,0b11110111
         out PORTB,r16
     delay1:
         dec r17
         brne delay1
+            dec r18
+            brne delay1
 	ldi r17,0xFF
+    ldi r18,0xFF
     led2:
         ldi r16,0b11101111
         out PORTB,r16
     delay2:
         dec r17
         brne delay2
+            dec r18
+            brne delay2
 	ldi r17,0xFF
+    ldi r18,0xFF
     led3:
         ldi r16,0b11011111
         out PORTB,r16
     delay3:
         dec r17
-        brne delay2
-	ldi r17,0xFF
+        brne delay3
+            dec r18
+            brne delay3
     rjmp main;
